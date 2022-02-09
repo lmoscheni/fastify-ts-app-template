2d7bf0f77213e03fa08342805770776c89bf2410
Leandro Moscheni <leandromoscheni@gmail.com>
2022-02-09 17:30:26 -0300

    Use new Configuration and add MongoDbConnection start on main

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,14 +1,30 @@
-import 'reflect-metadata'; // Enable the use of annotations/decorators
+import 'reflect-metadata' // Enable the use of annotations/decorators
 
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-import App from './app';
+import App from './app'
 
-dotenv.config();
+import MongoDbConnection from '@utils/MongoDbConnection'
+import { getRequestContext } from '@utils/RequestContext'
+import { Configuration } from '@config/index'
+
+dotenv.config()
+Configuration.init()
 
 function main(): void {
-  const app = new App();
-  app.start();
+  const context = getRequestContext()
+
+  const app = new App()
+
+  MongoDbConnection.connect()
+    .then(() => {
+      app.start()
+    })
+    .catch((err) => {
+      context.logger.error(
+        `An error has ocurred at connect to mongo client ${err.toString()}`
+      )
+    })
 }
 
-main();
+main()
