5fc0bde7fbc31efc77be05490b32f9a40f21c3c8
Leandro Moscheni <leandromoscheni@gmail.com>
2022-02-09 17:27:50 -0300

    Refactor Configuration Object

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,27 @@
 type Config = {
-  port: number;
-  environment: string;
-};
+  port: number
+  environment: string
+  databaseUrl: string
+  dbName: string
+}
 
-export default (): Config => ({
-  port: Number(process.env.PORT) || 5000,
-  environment: String(process.env.NODE_ENV) || 'development'
-});
+export class Configuration {
+  static instance: Config
+
+  private constructor() {
+    throw new Error("You never musn't use this constructor")
+  }
+
+  static init(): void {
+    Configuration.instance = {
+      port: Number(process.env.PORT) || 5000,
+      environment: String(process.env.NODE_ENV) || 'development',
+      databaseUrl: String(process.env.DATABASE_URL),
+      dbName: String(process.env.DB_NAME)
+    }
+  }
+
+  static getInstance(): Config {
+    return Configuration.instance
+  }
+}
